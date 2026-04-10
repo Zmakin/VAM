cb39f02c0b92b21ebe94999d64ba3118b9c4494c
Zmakin <114831560+Zmakin@users.noreply.github.com>
2026-04-09 21:15:59 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/VAM/',
+    base: '/your_repo_name_here/', // Make sure to replacce with your github.com/User_Name/Repo_Name, only after User_Name/
 });
